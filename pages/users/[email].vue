7e57a3441daaaf38a0af822732d230eca8241efc
kubgus <ja.gustafik@outlook.sk>
2023-07-03 17:50:15 +0200

    API fetching

---
+++ pages/users/[email].vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 class="text-3xl capitalize">{{ user.name.firstname }} {{ user.name.lastname }}</h1>
+        <div class="mt-3 gap-2">
+            <p>Email: {{ user.email }}</p>
+            <p>Password: {{ user.password }}</p>
+            <p>Phone: {{ user.phone }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const { email } = useRoute().params;
+    const uri = "https://fakestoreapi.com/users/" + email;
+
+    const { data: user } = await useFetch(uri, { key: email });
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
-
+        <div class="grid grid-cols-4 gap-5">
+            <div v-for="u in users" :key="u.id">
+                <NuxtLink :to="`/users/${u.id}`">{{ u.email }}</NuxtLink>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
-
+    const { data: users } = await useFetch("https://fakestoreapi.com/users");
 </script>
 
 <style lang="scss" scoped>
